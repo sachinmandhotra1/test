2334244c2091c1c0d9db31b10dafbb0de51fa90a
Sachin Mandhotra <163806857+sachinmandhotra1@users.noreply.github.com>
2024-11-29 13:44:54 +0530

    Add files via upload

---
+++ git_test.py
@@ -0,0 +1,32 @@
+from prefect import flow, task
+from prefect.cache_policies import NONE
+from datetime import datetime
+
+
+@task(log_prints=True, cache_policy=NONE)
+def orchestrate_entity_workflow(entity_to_process, input_data):
+    """
+    Orchestrates the workflow for processing an entity and its related entities.
+
+    Parameters
+    ----------
+    entity_to_process : str
+        The name of the entity to be processed.
+    input_data : dict
+        A dictionary containing the data to be inserted for the entity.
+
+    Returns
+    -------
+    None
+    """
+    print(f"========= START TIME -> {datetime.now()} =========")
+    print(f"Processing entity - '{entity_to_process}'...")
+
+
+    print(f"Processed entity - '{entity_to_process}' successfully.")
+    print(f"========= END TIME -> {datetime.now()} =========")
+
+
+@flow(log_prints=True)
+def main(entity_to_process, input_data):
+    orchestrate_entity_workflow(entity_to_process, input_data)
