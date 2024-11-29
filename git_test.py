ae1bbfd946288e80488aa3046d5e4dfe6f77cf65
Sachin Mandhotra <163806857+sachinmandhotra1@users.noreply.github.com>
2024-11-29 14:18:38 +0530

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -1,9 +1,10 @@
 from prefect import flow, task
 from prefect.cache_policies import NONE
+from datetime import datetime
 
 
 @task(log_prints=True, cache_policy=NONE)
-def orchestrate_entity_workflow(entity_to_process, input_data):
+def orchestrate_entity_workflow(entity_to_process=None, input_data=None):
     """
     Orchestrates the workflow for processing an entity and its related entities.
 
@@ -18,12 +19,12 @@ def orchestrate_entity_workflow(entity_to_process, input_data):
     -------
     None
     """
-    print(f"========= START TIME -> =========")
+    print(f"========= START TIME -> {datetime.now()} =========")
     print(f"Processing entity - '{entity_to_process}'...")
 
 
     print(f"Processed entity - '{entity_to_process}' successfully.")
-    print(f"========= END TIME -> =========")
+    print(f"========= END TIME -> {datetime.now()} =========")
 
 
 @flow(log_prints=True)
